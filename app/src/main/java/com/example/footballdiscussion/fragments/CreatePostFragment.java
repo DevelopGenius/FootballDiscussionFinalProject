e61689e363e64e585d1e27dc00ab2343dfb60f0d
ariel <ariel3332@gmail.com>
2023-02-12 23:40:10 +0200

    Changing the camera, gallery buttons and image fit

--- app/src/main/java/com/example/footballdiscussion/fragments/CreatePostFragment.java
+++ app/src/main/java/com/example/footballdiscussion/fragments/CreatePostFragment.java
@@ -5,7 +5,6 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -14,14 +13,11 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.footballdiscussion.R;
 import com.example.footballdiscussion.databinding.FragmentCreatePostBinding;
-import com.example.footballdiscussion.databinding.FragmentUserPostsBinding;
 import com.example.footballdiscussion.models.entities.User;
 import com.example.footballdiscussion.models.entities.UserPost;
 import com.example.footballdiscussion.view_modals.UserPostsViewModel;
@@ -32,16 +28,26 @@ import java.util.UUID;
 public class CreatePostFragment extends Fragment {
     private UserPostsViewModel viewModel;
     private FragmentCreatePostBinding binding;
+    ActivityResultLauncher<Void> cameraLauncher;
     ActivityResultLauncher<String> galleryLauncher;
     boolean isPostImageSelected = false;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        cameraLauncher = registerForActivityResult(new ActivityResultContracts.TakePicturePreview(), new ActivityResultCallback<Bitmap>() {
+            @Override
+            public void onActivityResult(Bitmap result) {
+                if (result != null) {
+                    binding.newPostImg.setImageBitmap(result);
+                    isPostImageSelected = true;
+                }
+            }
+        });
         galleryLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
             @Override
             public void onActivityResult(Uri result) {
-                if (result != null){
+                if (result != null) {
                     binding.newPostImg.setImageURI(result);
                     isPostImageSelected = true;
                 }
@@ -49,39 +55,12 @@ public class CreatePostFragment extends Fragment {
         });
 
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentCreatePostBinding.inflate(inflater, container, false);
-
-        binding.newPostGalleryButton.setOnClickListener(view->{
-            galleryLauncher.launch("image/*");
-        });
-
-        binding.publishPostButton.setOnClickListener(view -> {
-            String title = binding.postTitleEt.getText().toString();
-            User currentUser = viewModel.getCurrentUser();
-            String postId = currentUser.getId() + "_"  +UUID.randomUUID().toString();
-            UserPost userPost = new UserPost(postId,currentUser.getId(), currentUser.getUsername(),title, "" );
-
-            if (isPostImageSelected){
-                binding.newPostImg.setDrawingCacheEnabled(true);
-                binding.newPostImg.buildDrawingCache();
-                Bitmap bitmap = ((BitmapDrawable) binding.newPostImg.getDrawable()).getBitmap();
-               viewModel.uploadImage(postId, bitmap, url->{
-                    if (url != null){
-                        userPost.setImageUrl(url);
-                    }
-                   viewModel.publishUserPost(userPost, (unused) -> {
-                        Navigation.findNavController(view).navigate(R.id.action_createPostFragment_to_ownUserPostsFragment);
-                    });
-                });
-            }else {
-                viewModel.publishUserPost(userPost, (unused) -> {
-                    Navigation.findNavController(view).navigate(R.id.action_createPostFragment_to_ownUserPostsFragment);
-                });
-            }
-        });
+        setListeners();
         return binding.getRoot();
     }
 
@@ -91,5 +70,40 @@ public class CreatePostFragment extends Fragment {
         super.onAttach(context);
         viewModel = new ViewModelProvider(this).get(UserPostsViewModel.class);
 
+    }
+
+    private void setListeners() {
+        binding.newPostGalleryButton.setOnClickListener(view -> {
+            galleryLauncher.launch("image/*");
+        });
+
+        binding.newPostCameraButton.setOnClickListener(view1 -> {
+            cameraLauncher.launch(null);
+        });
+
+        binding.publishPostButton.setOnClickListener(view -> {
+            String title = binding.postTitleEt.getText().toString();
+            User currentUser = viewModel.getCurrentUser();
+            String postId = currentUser.getId() + "_" + UUID.randomUUID().toString();
+            UserPost userPost = new UserPost(postId, currentUser.getId(), currentUser.getUsername(), title, "");
+
+            if (isPostImageSelected) {
+                binding.newPostImg.setDrawingCacheEnabled(true);
+                binding.newPostImg.buildDrawingCache();
+                Bitmap bitmap = ((BitmapDrawable) binding.newPostImg.getDrawable()).getBitmap();
+                viewModel.uploadImage(postId, bitmap, url -> {
+                    if (url != null) {
+                        userPost.setImageUrl(url);
+                    }
+                    viewModel.publishUserPost(userPost, (unused) -> {
+                        Navigation.findNavController(view).navigate(R.id.action_createPostFragment_to_ownUserPostsFragment);
+                    });
+                });
+            } else {
+                viewModel.publishUserPost(userPost, (unused) -> {
+                    Navigation.findNavController(view).navigate(R.id.action_createPostFragment_to_ownUserPostsFragment);
+                });
+            }
+        });
     }
 }
